e1ec62457cde01e2ba6a1f5fb3548184890f1298
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2016-01-13 12:18:22 -0500

    more story paths, options and text compartmentalized

--- StoryExample/AppDelegate.swift
+++ StoryExample/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-var page1 = Page(pageText: "")
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,62 +24,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         StoryCore.theStory = Story(startPage: page1)
         StoryCore.currPage = page1
 
-        //create the options for our first page
-        let page2 = Page(pageText: "After what seems like hours of exhausting hiking over several hills, a faint trickling sound reaches your ears. You hurry forward, eager to find it, as you are even thirstier than before. Suddenly, the trees part. The current is fast, but the water looks clear and feels blissfully cool when you dip your fingers in.")
-        let page3 = Page(pageText: "You close your eyes, spin in a circle three times, and start walking. Unfortunately, you trip over a rock and slam face-first into a tree trunk. Now, you have a broken ankle and a deep gash on your forehead. You can feel the warm, sticky blood slowly trickling down your face.")
-        let page4 = Page(pageText: "The side of the road is as good a place as any for a nap. You clear the sticks and dead leaves away from the roots of a large tree. When you are done, there is a surprisingly comfortable bare patch on the ground. You curl up against the tree and close your eyes.")
-        let page5 = Page(pageText: "You eagerly step into the street, forgetting to check for incoming vehicles. Not two steps into the street, an incredibly shiny sports car slams into you. As you slide off the hood, you unexpectedly find yourself saddened by the way your blood is staining the car's otherwise perfect paint... then your broken ribs register, and you start screaming.")
         page1.addOption(Option(optionText: "Search for the stream.", destinationPage: page2))
         page1.addOption(Option(optionText: "Walk in a random direction.", destinationPage: page3))
         page1.addOption(Option(optionText: "Take a nap.", destinationPage: page4))
         page1.addOption(Option(optionText: "Cross the street.", destinationPage: page5))
         
-        //add options to page 2
-        let page6 = Page(pageText: "The water is cool and refreshing. You drink as much as you can, but you soon notice that you are also hungry. You glance upward and spot a small cluster of spotted red mushrooms. The vibrant red colors seem a bit suspicious, but you've survived worse... right? Besides, you remember reading that some harmless plants use toxic-looking colors to scare away predators. It could be the same for fungi; the mushroom is probably edible.")
-        let page7 = Page(pageText: "")
-        let page8 = Page(pageText: "")
-        let page9 = Page(pageText: "")
         page2.addOption(Option(optionText: "Drink some water.", destinationPage: page6))
         page2.addOption(Option(optionText: "Go swimming.", destinationPage: page7))
         page2.addOption(Option(optionText: "Walk a bit more.", destinationPage: page8))
         page2.addOption(Option(optionText: "Poke around the riverbank.", destinationPage: page9))
-        
-        //add options to page 3
-        let page10 = Page(pageText: "")
-        let page11 = Page(pageText: "")
+
         page3.addOption(Option(optionText: "Try to stop the bleeding.", destinationPage: page10))
         page3.addOption(Option(optionText: "Start screaming in pain.", destinationPage: page11))
         
-        //add options to page 4
-        let page12 = Page(pageText: "")
-        let page13 = Page(pageText: "")
-        let page14 = Page(pageText: "")
-        page4.addOption(Option(optionText: "", destinationPage: page12))
-        page4.addOption(Option(optionText: "", destinationPage: page13))
-        page4.addOption(Option(optionText: "", destinationPage: page14))
-        
-        //add options to page 5
-        let page15 = Page(pageText: "You lie in the street, your organs spattered across the asphalt. Just as you think you can muster the strength to crawl away, you hear the revving of the sports car that hit you. In the car is an angry-looking man with a prominent tattoo of his name - Nate. He pauses for a single moment, during which time you desperately hope it will not hurt as much as the previous collision. You are wrong - it hurts more... but nothingness follows soon after.")
+        page4.addOption(Option(optionText: "Sleep for 2 hours.", destinationPage: page12))
+        page4.addOption(Option(optionText: "Sleep all night.", destinationPage: page13))
+        page4.addOption(Option(optionText: "Reflect on your situation.", destinationPage: page14))
+
         page5.addOption(Option(optionText: "Get re-in-car-Nate-'d!", destinationPage: page15))
         
-        //p6
-        let page16 = Page(pageText: "You throw caution to the wind and take a big bite. It is surprisingly tasty, and you don't feel sick at all. You hungrily pick the rest of the mushrooms and consume them.")
-        let page17 = Page(pageText: "You poke around the riverbank a bit, hungrily gathering a few plants together. One of them stings your hand when you touch it, so you smartly decide not to eat it. However, the others slightly resemble a tossed salad without any dressing. Your stomach grumbles again, telling you to hurry up and consume food.")
-        let page18 = Page(pageText: "")
         page6.addOption(Option(optionText: "Eat the mushrooms.", destinationPage: page16))
         page6.addOption(Option(optionText: "Find something else.", destinationPage: page17))
         page6.addOption(Option(optionText: "Drink more water.", destinationPage: page18))
         
-        //options for page 15
+        page13.addOption(Option(optionText: "Search for the stream.", destinationPage: page2))
+        page13.addOption(Option(optionText: "Wave down a car.", destinationPage: page22))
+        
+        page14.addOption(Option(optionText: "Go towards the car.", destinationPage: page21))
+        page14.addOption(Option(optionText: "Run the other direction.", destinationPage: page23))
+
         page15.addOption(Option(optionText: "You are dead. Reset?", destinationPage: page1))
         
-        //17
-        let page19 = Page(pageText: "")
-        let page20 = Page(pageText: "A wave of disgust sweeps through you, threatening to make you vomit. You chuck the handful of plants as far away as possible. There's no way you would ever eat vegetables, even if your life depended upon it! Besides, you remember hearing that humans can survive around three weeks without food. That's plenty of time to find something better to eat.")
-        let page21 = Page(pageText: "")
         page17.addOption(Option(optionText: "Eat the 'salad'.", destinationPage: page19))
         page17.addOption(Option(optionText: "Eww, vegetables.", destinationPage: page20))
-        page17.addOption(Option(optionText: "Look for meat.", destinationPage: page21))
+        page17.addOption(Option(optionText: "Look for meat.", destinationPage: page9))
+        
+        page21.addOption(Option(optionText: "See if the driver is ok.", destinationPage: page24))
+        page21.addOption(Option(optionText: "Steal the cash and run.", destinationPage: page25))
         
         // Override point for customization after application launch.
         return true
---
+++ StoryExample/storytext.swift
@@ -0,0 +1,38 @@
+//
+//  storytext.swift
+//  StoryExample
+//
+//  Created by Rosalyn Koscica on 1/13/16.
+//  Copyright © 2016 anna. All rights reserved.
+//
+
+import Foundation
+
+var page1 = Page(pageText: "")
+let page2 = Page(pageText: "After what seems like hours of exhausting hiking over several hills, a faint trickling sound reaches your ears. You hurry forward, eager to find it, as you are even thirstier than before. Suddenly, the trees part. The current is fast, but the water looks clear and feels blissfully cool when you dip your fingers in.")
+let page3 = Page(pageText: "You close your eyes, spin in a circle ten times, and start walking. Unfortunately, you trip over a rock and slam face-first into a tree trunk. Now, you have a broken ankle and a deep gash on your forehead. You can feel the warm, sticky blood slowly trickling down your face.")
+let page4 = Page(pageText: "The side of the road is as good a place as any for a nap. You clear the sticks and dead leaves away from the roots of a large tree. When you are done, there is a surprisingly comfortable bare patch on the ground. You curl up against the tree and close your eyes.")
+let page5 = Page(pageText: "You eagerly step into the street, forgetting to check for incoming vehicles. Not two steps into the street, an incredibly shiny sports car slams into you. As you slide off the hood, you unexpectedly find yourself disappointed by the way your blood is staining the car's otherwise pristine paint... then your broken ribs register, and you start screaming.")
+let page6 = Page(pageText: "The water is cool and refreshing. You drink as much as you can, but you soon notice that you are also hungry. You glance upward and spot a small cluster of spotted red mushrooms. The vibrant red colors seem a bit suspicious, but you've survived worse... right? Besides, you remember reading that some harmless plants use toxic-looking colors to scare away predators. It could be the same for fungi; the mushroom is probably edible.")
+let page7 = Page(pageText: "You wade in. The river is surprisingly shallow, and the water feels delightedly cool. You happily immerse yourself in the water, holding on to large rocks to keep from being swept away by the current. After a while, you find yourself feeling rather cold. You tiredly drag yourself out of the river, only to find some strange, jellylike blobs attached to your skin.")
+let page8 = Page(pageText: "You walk until your feet are aching, but you only find yourself thirstier than before. Plus, you now have no idea where you are in relation to the river. Great going, you.")
+let page9 = Page(pageText: "You flip a few rocks over. After a while, you have ammassed quite a collection of small fish, worms, and crayfish. You are still thirsty, but you hesitate to drink the river water after finding all these critters.")
+let page10 = Page(pageText: "You gather together some spongy-looking leaves and press them against your wound, desperately hoping that they are not toxic.")
+let page11 = Page(pageText: "You start screaming loudly, though you doubt anyone passing on the noisy highway will hear you through the thick plate glass. You don't know what you are hoping to accomplish, but the increased stress makes your blood pressure rise. Soon, the wound is bleeding even faster... and you have exhausted yourself.")
+let page12 = Page(pageText: "You drift off to sleep... ")
+let page13 = Page(pageText: "You open your eyes to see bright sunlight filtering through the lush green canopy above you. The night has been uneventful, and now you feel refreshed. However, you are more thirsty than you were yesterday, and you also feel slightly hungry.")
+let page14 = Page(pageText: "As you are drifting off to sleep, you wonder what might happen if a car were to swerve off the road and hit your tree. Suddenly, you don't feel so tired anymore. You leap to your feet, adrenaline coursing through your veins, and sprint into the forest. Moments later, you hear the screech of tires as a shiny sports car slams into the tree, splitting the thick trunk and obliterating the comfortable spot you were in just moments before. Whew, that was a close one!")
+let page15 = Page(pageText: "You lie in the street, your organs spattered across the asphalt. Just as you think you can muster the strength to crawl away, you hear the revving of the sports car that hit you. In the car is an angry-looking man with a prominent tattoo of his name - Nate. He pauses for a single moment, during which time you desperately hope it will not hurt as much as the previous collision. You are wrong - it hurts more... but nothingness follows soon after.")
+let page16 = Page(pageText: "You throw caution to the wind and take a big bite. It is surprisingly tasty, and you don't feel sick at all. You hungrily pick the rest of the mushrooms and consume them.")
+let page17 = Page(pageText: "You poke around the riverbank a bit and find several plants that look vaguely edible. One of them stings your hand when you rub its leaves, so you smartly decide to get rid of it. However, the others slightly resemble a tossed salad without any dressing. Your stomach grumbles, telling you to hurry up and consume food.")
+let page18 = Page(pageText: "")
+let page19 = Page(pageText: "")
+let page20 = Page(pageText: "A wave of disgust sweeps through you, threatening to make you vomit. You chuck the handful of plants as far away as possible. There's no way you would ever eat vegetables, even if your life depended upon it! Besides, you remember hearing that humans can survive around three weeks without food. That's plenty of time to find something better to eat.")
+let page21 = Page(pageText: "You approach the wreckage. The ruined sports car has tinted windows, so you must lean close and cup your hands around your eyes in order to see anything. You spot a slumped figure on the driver's side, though its features are obscured by the airbag. On the passenger side, several bundles of cash spill from a tear in a large sack.")
+let page22 = Page(pageText: "")
+let page23 = Page(pageText: "")
+let page24 = Page(pageText: "")
+let page25 = Page(pageText: "")
+let page26 = Page(pageText: "")
+let page27 = Page(pageText: "")
+let page28 = Page(pageText: "")
